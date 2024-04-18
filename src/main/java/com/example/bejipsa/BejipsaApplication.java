3d08f812c97037f12fd09af12c1ca1a0ca3e4b0c
gyesswhat <h2weirdo@icloud.com>
2024-04-18 21:27:19 +0900

    chore: 기본 파일

---
+++ src/main/java/com/example/bejipsa/BejipsaApplication.java
@@ -0,0 +1,13 @@
+package com.example.bejipsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BejipsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BejipsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bejipsa/BejipsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bejipsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BejipsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
